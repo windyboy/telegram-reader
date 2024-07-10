5a5d78854273f8b1f5625417589fb87b0b4528b8
windyboy <windyboy@gmail.com>
2024-07-10 17:01:57 +0800

    chore: Refactor Telegram module tests and add test cases for GetTelegramFromText

--- telegram/telegram_test.go
+++ telegram/telegram_test.go
@@ -16,7 +16,7 @@ func TestTelegram(t *testing.T) {
 }
 
 var _ = Describe("Telegram", func() {
-	Context("Given a telegram string", func() {
+	Context("Telegram test mode", func() {
 		var sugar *zap.SugaredLogger
 
 		BeforeEach(func() {
@@ -27,7 +27,7 @@ var _ = Describe("Telegram", func() {
 			SetSugaredLogger(sugar)
 		})
 
-		It("should return the sequence when the telegram is matched by the sequence pattern", func() {
+		Context("A complete telegram string", func() {
 			text := `ZCZC TMQ2627 151600
 
 
@@ -46,9 +46,101 @@ FF ZBTJZXZX
 
 
 NNNN`
-			seqPattern := "ZCZC\\s(\\S+)\\s"
-			seq := GetTelegramSequence(text, seqPattern)
-			Expect(seq).To(Equal("TMQ2627"))
-		})
-	})
-})
+
+			It("should return the sequence when the telegram is matched by the sequence pattern", func() {
+
+				seqPattern := "ZCZC\\s(\\S+)\\s"
+				seq := GetTelegramSequence(text, seqPattern)
+				Expect(seq).To(Equal("TMQ2627"))
+			})
+		}) //End of Context
+
+		Context("A string with 2 telegrams", func() {
+			text := `
+ZCZC TMQ2611 151524
+
+
+FF ZBTJZPZX
+
+
+151524 ZGGGZPZX
+
+
+(ARR-CSN3136/A0006-ZBTJ-ZGGG1521)
+
+
+
+
+
+
+
+NNNN
+
+ZCZC TMQ2609 151524
+
+
+FF ZBTJZPZX
+
+
+151523 ZBACZQZX
+
+
+(ARR-CXA8324/A4001-ZLXY-ZBTJ1522)
+
+
+
+
+
+
+
+NNNN
+			`
+			pattern := "(?s)ZCZC.*?NNNN"
+			telegrams := GetTelegramFromText(text, pattern)
+			t1 := `ZCZC TMQ2611 151524
+
+
+FF ZBTJZPZX
+
+
+151524 ZGGGZPZX
+
+
+(ARR-CSN3136/A0006-ZBTJ-ZGGG1521)
+
+
+
+
+
+
+
+NNNN`
+			t2 := `ZCZC TMQ2609 151524
+
+
+FF ZBTJZPZX
+
+
+151523 ZBACZQZX
+
+
+(ARR-CXA8324/A4001-ZLXY-ZBTJ1522)
+
+
+
+
+
+
+
+NNNN`
+			It("should return 2 telegrams", func() {
+				Expect(len(telegrams)).To(Equal(2))
+				Expect(telegrams[0]).To(Equal(t1))
+				Expect(telegrams[1]).To(Equal(t2))
+
+			})
+
+		}) //End of Context
+
+	}) //End of Describe
+}) //End of Context
